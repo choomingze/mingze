3c77e5206018c37b216043e02adc917fd7cc4dc7
mingggze <74172369+mingggze@users.noreply.github.com>
2024-01-28 22:50:51 +0000

    added pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 import { NavBar } from './components/NavBar';
-
+import { Skills } from './components/Skills';
+import { AboutMe } from './components/AboutMe';
+import { Experience } from './components/Experience';
+import { Project } from './components/Project';
+import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <div className="App">
       <NavBar/>
-    </div>
+      <AboutMe/>
+      <Skills/>
+      <Experience/>
+      <Project/>    
+      </div>
   );
 }
 
